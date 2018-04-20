b623e584cab5dba7f81d4e674b5bcb98e65f473a
susomejias <jesusmejias.jm@gmail.com>
2018-04-20 18:25:29 +0200

    Añadido creacion tabla jornada laboral, creacion secuencia, y trigger para crear id autoincrementables

---
+++ PL-SQL/CREACION_TABLA_JORNADA_LABORAL.sql
@@ -0,0 +1,11 @@
+CREATE TABLE JORNADALABORAL (
+
+  id integer,
+  fecha_inicio date,
+  fecha_final date,
+  hora_inicio date,
+  hora_fin date,
+
+  primary key (id)
+
+);
---
+++ PL-SQL/crearSecuencia.pkb
@@ -0,0 +1,2 @@
+-- Crear secuencia para poder hacer un autoincrement
+CREATE SEQUENCE ID_AUTOINCREMENT START WITH 1;
